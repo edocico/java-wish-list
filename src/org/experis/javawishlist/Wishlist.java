0e89762d5529136771655d5b7f40d32a65a57a5d
edocico <138378767+edocico@users.noreply.github.com>
2024-04-05 15:03:00 +0200

    made wishlist

--- src/org/experis/javawishlist/Wishlist.java
+++ src/org/experis/javawishlist/Wishlist.java
@@ -1,7 +1,43 @@
 package org.experis.javawishlist;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Wishlist {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        List<Gift> gifts = new ArrayList<>();
+
+        int counter = 0;
+
+        boolean exit = false;
+
+        do {
+            System.out.println("Vuoi inserire un altro regalo nella lista ? s/n");
+            String choise = scan.nextLine();
+
+            switch (choise.toUpperCase()) {
+                case "S":
+                    System.out.println("Inserisci un regalo");
+                    String answer = scan.nextLine();
+                    String answerClean = answer.trim();
+                    Gift gift2 = new Gift(answerClean);
+                    gifts.add(gift2);
+                    System.out.println("Hai aggiunto " + gifts.size() +  " regali alla lista");
+                    break;
+                case "N":
+                    exit = true;
+                    System.out.println("Hai aggiunto " + gifts.size() +  " regali alla lista");
+                    break;
+                default:
+                    System.out.println("comando non valido");
+                    break;
+            }
+        }while (!exit);
+
+        scan.close();
     }
 }
